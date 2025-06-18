70f4d13e8742bb30dce903f1acc4d9344bdac482
parth-kohli <parthdabesthuman@gmail.com>
2025-06-18 17:31:22 +0530

    Delete scripts directory

--- scripts/request_author.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-username="${SUDO_USER:-USER}"
-file="/home/admin/requests.yaml"
-if sudo grep -qw "$username" "$file"; then
- 	echo "You have already requested"
-  	exit 1
-fi
-echo "- $username" | sudo tee -a "$file" > /dev/null
-echo "Request sent"
